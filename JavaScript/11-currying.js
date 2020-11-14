184612b2e5b4a1e20a56ecdacf39425029ef87be
Zps <287879667@qq.com>
2020-11-14 21:45:52 +0800

    commit

--- JavaScript/11-currying.js
+++ JavaScript/11-currying.js
@@ -1,24 +1,24 @@
 // 简单实现
 
 function createCurry(func, args) {
-  var arity = func.length
-  var args = args || []
+  var arity = func.length;
+  var args = args || [];
 
-  return function() {
-    var _args = [].slice.call(arguments)
-    ;[].push.apply(_args, args)
+  return function () {
+    var _args = [].slice.call(arguments);
+    [].push.apply(_args, args);
 
-    if(_args.length < arity) {
-      return createCurry.call(this, func, _args)
+    if (_args.length < arity) {
+      return createCurry.call(this, func, _args);
     }
 
     // 参数收集完毕
-    return func.apply(this, _args)
-  }
+    return func.apply(this, _args);
+  };
 }
 
 function multiply(a, b, c) {
-  return a * b * c
+  return a * b * c;
 }
 
 // 编写可以轻松复用和配置的小代码块
@@ -28,16 +28,13 @@ function multiply(a, b, c) {
 //   return price * discount
 // }
 // 柯里化之后
-function discount(discount){
-  return price => price * discount
+function discount(discount) {
+  return (price) => price * discount;
 }
 
-const tenPercentDiscount = discount(0.1)
-console.log(tenPercentDiscount(500))
-console.log(tenPercentDiscount(300))
-const twentyPercentDiscount = discount(0.2)
-console.log(twentyPercentDiscount(500))
-console.log(twentyPercentDiscount(300))
-
-
-function composite()
+const tenPercentDiscount = discount(0.1);
+console.log(tenPercentDiscount(500));
+console.log(tenPercentDiscount(300));
+const twentyPercentDiscount = discount(0.2);
+console.log(twentyPercentDiscount(500));
+console.log(twentyPercentDiscount(300));
